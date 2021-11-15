4861cea79ba866b58becfbd018f8020ecd3d1d09
Sven Fischer <git-dev@linux4tw.de>
2021-11-15 23:26:23 +0100

    delete init-db.flag only on successful db initialization

--- osmbc/entrypoint.sh
+++ osmbc/entrypoint.sh
@@ -2,8 +2,7 @@
 
 if [ -f init-db.flag ]; then
    echo "First start, creating database."
-   node ./import/createdb.js --createTables --dropTables --createView --createIndex --addUser $OSMUSERNAME --verbose
-   rm init-db.flag
+   node ./import/createdb.js --createTables --dropTables --createView --createIndex --addUser $OSMUSERNAME --verbose && rm init-db.flag
 fi
 
 # startup
