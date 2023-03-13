fb87f69c384728b504b9471155dbe5a8d0541398
Matthias <matthias@pebble>
2023-03-13 23:48:07 +0100

    setup for local with dbdump and login via htpasswd
    Signed-off-by: Matthias <matthias@pebble>

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Development environment for the [OSM Blog Collector](https://github.com/TheFive/osmbc)
+
+Provides the OSM Blog Collector locally at https://localhost:35043 - self-signed certificates have to be accepted.
+
+Currently no demo database can be created, a dump is required. For login, the user `test` with password `test` can be used via `htaccess` method, but `access` needs to be set to `full` in postgres `usert` manually for full functionality.
--- osmbc/entrypoint.sh
+++ osmbc/entrypoint.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
-if [ -f init-db.flag ]; then
+if [ ! -f db-ready.flag ]; then
    echo "First start, creating database."
-   node ./import/createdb.js --createTables --dropTables --createView --createIndex --addUser $OSMUSERNAME --verbose && rm init-db.flag
+   node ./import/createdb.js --createTables --dropTables --createView --createIndex --addUser $OSMUSERNAME --verbose # && touch db-ready.flag
+   # workaround: import database from dump as above not working :-/
+   #apt update
+   #apt -y install postgresql-client
+   #psql --host db postgres -U postgres < osmbc_data.pgdump
+   touch db-ready.flag
 fi
 
 # startup
