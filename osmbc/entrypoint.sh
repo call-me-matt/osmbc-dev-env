d178e6afe5e5dceb37eb3285963cd53e5cefe995
Matthias <matthias@matthiasheinisch.de>
2023-03-23 21:25:27 +0100

    osmbc updated, remove workaround
    Signed-off-by: Matthias <matthias@matthiasheinisch.de>

--- osmbc/entrypoint.sh
+++ osmbc/entrypoint.sh
@@ -2,9 +2,10 @@
 
 if [ ! -f db-ready.flag ]; then
    echo "First start, creating database."
-   node ./import/createdb.js --createTables --dropTables --createView --createIndex --verbose --addUser $OSMUSERNAME && touch db-ready.flag
+   node ./import/createdb.js --createTables --dropTables --createView --createIndex --verbose --addUser $OSMUSERNAME
    echo "add local login with password 'test'"
    echo "$OSMUSERNAME:\$2y\$05\$WbBcRVSTP5nOPhcQs6HTguemtIJrkKAUvElBZ7Kkme5R7rBn6e92a" > /var/www/osmbc/test_pwd
+   touch db-ready.flag
 fi
 
 # startup
